c43c80e96e85ab2e75310862ef6eaca886cd244e
Jai Parasher <jaiparasher30@gmail.com>
2024-04-13 22:47:11 +0530

    Add Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Layout from "./Layout";
 import HomePage from "./pages/HomePage";
+import { Toaster } from "react-hot-toast";
+import Login from "./components/Login";
 
 function App() {
     return (
@@ -10,7 +12,21 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route path="" element={<HomePage />}/>
                 </Route>
+                <Route path="/login" element={<Login />}/>
             </Routes>
+            <Toaster
+                position="top-right"
+                reverseOrder={true}
+                toastOptions={{
+                    error: {
+                        style: { borderRadius: "0", color: "red",},
+                    },
+                    success: {
+                        style: { borderRadius: "0", color: "green" },
+                    },
+                    duration: 2000
+                }}
+            />
         </>
     );
 }
